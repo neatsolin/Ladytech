475c7065cee75008e8425ff65a81bb3ee88a771b
Oeub ROCHOM <oeub.rochom@student.passerellesnumeriques.org>
2025-03-09 11:53:41 +0700

    feat/(add product): added and cut home dashboard [DN-19]

--- views/layouts/admin/footer.php
+++ views/layouts/admin/footer.php
@@ -216,7 +216,7 @@
     <!-- [Page Specific JS] start -->
     <!-- Apex Chart -->
     <script src="../../../assets/js/plugins/apexcharts.min.js"></script>
-    <script src="../../../assets/js/pages/dashboard-analytics.js"></script>
+    <script src="../../../assets/js/pages/dashboard-default.js"></script>
     <!-- [Page Specific JS] end -->
     </body>
     <!-- [Body] end -->
