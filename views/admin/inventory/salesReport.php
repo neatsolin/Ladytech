ac046d26dc8f8e50467cac6c48850072965fac76
yongsy-17 <yongsy.din@student.passerellesnumeriques.org>
2025-03-20 17:29:31 +0700

    feat:sale_report

--- views/admin/inventory/salesReport.php
+++ views/admin/inventory/salesReport.php
@@ -1,8 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  
     <title>Sales Report</title>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
